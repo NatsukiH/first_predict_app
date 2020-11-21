e8507d102167ba496edd69d9b0054727af756640
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-11-21 22:34:09 +0900

    flaskを試してみた2

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
- 
-@app.route('/')
-def hello_world():
-    return '<html><body><h1>sample</h1></body></html>'
- 
-if __name__ == '__main__':
+from app.app import app
+
+if __name__ == "__main__":
     app.run()
