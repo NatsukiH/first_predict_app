a595125fc26b2b0ea0ef1bf2277297887d1488c1
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-11-23 23:27:09 +0900

    ポートの変更

--- app/app.py
+++ app/app.py
@@ -104,7 +104,7 @@ def downloads_file():
 
 # おまじない
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
