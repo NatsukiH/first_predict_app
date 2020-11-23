f517e78b2b453d950c78e0af63afad81b496972c
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2020-11-24 01:09:00 +0900

    run.pyにポート番号のコードを追加

--- app/app.py
+++ app/app.py
@@ -104,7 +104,4 @@ def downloads_file():
 
 # おまじない
 if __name__ == "__main__":
-    # app.run(debug=True)
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True)
